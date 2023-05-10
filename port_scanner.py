872a0365eda286ec5fc90ebee2ba8161e0a37c5c
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-05-10 04:11:20 +0200

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -13,5 +13,22 @@ else:
 
 print("-" * 50)
 print("Scanning Target: " * target)
+print("Scanning started at:" * str(datatime.now()))
 print("-" * 50)
 
+try:
+    
+    for port in range(1, 100):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        
+        result = s.connect_ex((target, port))
+        if result == 0:
+            print(port {} is open".format(port))
+        s.close()
+                  
+except KeyboardInterrupt:
+        print("\n Exitting Programme !")
+        sys.exit()
+except socket.gaierror:
+        print("\n Hostname Could Not Be Resolved !")           
