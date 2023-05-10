eb5fe6812f7f256243844ab0c0b2575ada8a8093
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-05-10 03:52:18 +0200

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -5,3 +5,8 @@ from datatime import datatime
 
 ascii_banner = pyfiglet.figlet_format("PORT SCANNER FROM PYTHON 3")
 print(ascii_banner)
+
+if len(sys.argv) == 2:
+    target = socket.gethostbyname(sys.argv[1])
+else:
+  print("Invalid amount of Argument you must enter the IP")
